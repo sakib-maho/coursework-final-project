17127b526ab9096f8765f0cfc4771a1b9a8a248d
Md Robiul Islam Sakib <70893762+sakib-maho@users.noreply.github.com>
2025-10-23 15:07:22 +0900

    Fix copyright and license issues
    Applied fixes:
    - Added LICENSE file
    - Added copyright headers to 2 files
    - Added .gitignore file
    - Added README.md file

--- imageLink.py
+++ imageLink.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 sakib-maho
+# Licensed under the MIT License
+# See LICENSE file for details
+
 from html.parser import HTMLParser
 import urllib.request
 from pprint import pprint
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 sakib-maho
+# Licensed under the MIT License
+# See LICENSE file for details
+
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
